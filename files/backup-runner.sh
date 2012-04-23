7145033e919e934d737eccdc265b073bc1e59bf6
Udo Waechter <root@zoide.net>
2012-04-23 23:49:58 +0200

    moved files one up. new repo-root

---
+++ files/backup-runner.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+## actually run duply
+BCKPNAME="$1"
+LOCKFILE="/tmp/lock-${BCKPNAME}"
+STATFILE="/var/log/.success-${BCKPNAME}"
+ACTION="$2"
+
+[[ -e $LOCKFILE ]] && exit 0 #exit if lockfile exists
+touch $LOCKFILE
+[[ -d /var/log/backup ]] || mkdir /var/log/backup
+/usr/bin/duply $BCKPNAME $ACTION >>/var/log/backup/backup-${BCKPNAME}.log 2>>/var/log/backup/backup-${BCKPNAME}_error.log
+if [ $? -eq 0 ]; then
+    touch $STATFILE
+else
+rm $LOCKFILE
