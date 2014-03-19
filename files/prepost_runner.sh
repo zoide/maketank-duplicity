d74e821ffd510b937d53947fc9b43b7ae1fdf19a
do <udo@maketank.net>
2014-03-19 19:48:25 +0100

    prepostrunner fixes

--- files/prepost_runner.sh
+++ files/prepost_runner.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-ddir=$1.d
-if [ "${ddir}" != "pre.d" ] || [ "${ddir}" != "post.d" ]; then
-    #Iterate through pre.d and execute em all
-    if [ ! -d "${ddir}" ]; then
-        echo "${ddir} does not exist" 
-        exit 1
-    fi
-
-    for PROG in ${ddir}/*.sh; do
-        echo "Doing: '${PROG}'"
-        ${PROG} 
-    done
-else
-    echo "Must be either 'pre' or 'post'! Error in prepost_runner.sh"
-    exit 1
+ddir=$1
+#Iterate through pre.d and execute em all
+if [ ! -d "${ddir}" ]; then
+     echo "${ddir} does not exist" 
+     exit 1
 fi
+
+for PROG in ${ddir}/*.sh; do
+     echo "Doing: '${PROG}'"
+     ${PROG} 
+done
